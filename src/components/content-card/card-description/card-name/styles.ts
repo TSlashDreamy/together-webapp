0e8960cea4f00c907b5551b7d4acbe6ab934e681
TSlashDreamy <supergamerzxtata@gmail.com>
2024-05-24 15:37:39 +0300

    Fixed content cards long names overflow

--- src/components/content-card/card-description/card-name/styles.ts
+++ src/components/content-card/card-description/card-name/styles.ts
@@ -1,3 +1,4 @@
-export const wrapperStyle = "xl:gap-[0.6vw] flex gap-[10px] w-full items-center justify-between overflow-hidden group-hover:overflow-visible";
+export const wrapperStyle =
+  "flex flex-wrap gap-0 w-full items-center justify-between overflow-hidden group-hover:overflow-visible xl:group-hover:gap-[0.6vw] group-hover:gap-[10px] transition-all";
 export const nameStyle = "xl:text-[1.1vw] text-[20px] font-medium text-nowrap group-hover:text-wrap";
-export const lengthStyle = "xl:text-[0.77vw] flex items-center text-[15px] opacity-0 overflow-hidden group-hover:opacity-100 transition-all";
+export const lengthStyle = "xl:text-[0.77vw] h-0 flex items-center text-[15px] opacity-0 overflow-hidden group-hover:opacity-100 group-hover:h-auto transition-all";
